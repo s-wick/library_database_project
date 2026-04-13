c4c3bf78b679250492d2e92af7d77e86ca709c3e
Sahan Wickramaratne <git@sahanwick.anonaddy.com>
2026-04-12 19:49:04 -0500

    new date-updater script logic, removed unnecessary parameters and applied to room-bookings

--- database/data/room-bookings.sql
+++ database/data/room-bookings.sql
@@ -1,12 +1,24 @@
+-- sample-base-date: 2026-04-12
+-- sample-shift-days: 0
+
 INSERT INTO meeting_room (room_number, capacity, has_projector, has_whiteboard, has_tv)
 VALUES
  ('101', 4, 1, 1, 1),
  ('102', 6, 0, 1, 1),
  ('201', 8, 1, 0, 0);
 
+-- sample-shift-days-next: 0
 INSERT INTO book_room (room_number, user_id, start_time, duration_hours)
 VALUES
- ('101', 1, '2026-04-15 10:00:00', 2),
- ('101', 2, '2026-04-15 13:00:00', 1),
- ('102', 3, '2026-04-16 09:00:00', 3);
+ ('101', 1, '2026-04-12 10:00:00', 2);
+
+-- sample-shift-days-next: 0
+INSERT INTO book_room (room_number, user_id, start_time, duration_hours)
+VALUES
+ ('101', 2, '2026-04-12 13:00:00', 1);
+
+-- sample-shift-days-next: 0
+INSERT INTO book_room (room_number, user_id, start_time, duration_hours)
+VALUES
+ ('102', 3, '2026-04-13 09:00:00', 3);
 

9b80e8c75bcc7b205decf3ecd6f971c954cc05bf
Sahan Wickramaratne <112655106+s-wick@users.noreply.github.com>
2026-04-13 23:49:59 +0000

    Hide login credentials (#70)
    Hide passwords in `users.sql` and edit `db:reset` script to match. Also
    updated workflow.
    
    # Changes
    
    - New `.env.example` to store staff/user passwords
    - Hidden passwords in SQL insert statements
    - Edited `reset-database.js` script to replace with passwords

--- database/data/users.sql
+++ database/data/users.sql
@@ -2,11 +2,11 @@
 
 INSERT INTO staff_account (email, password, first_name, middle_name, last_name, phone_number, is_admin)
 VALUES
- ('librarian.staff@lib.com', 'password123', 'Lauren', NULL, 'Mitchell', '555-0101', 0),
- ('admin.staff@lib.com', 'password123', 'Rafael', NULL, 'Torres', '555-0102', 1);
+ ('librarian.staff@lib.com', {{SEED_STAFF_PASSWORD}}, 'Lauren', NULL, 'Mitchell', '555-0101', 0),
+ ('admin.staff@lib.com', {{SEED_STAFF_PASSWORD}}, 'Rafael', NULL, 'Torres', '555-0102', 1);
 
 INSERT INTO user_account (email, password, first_name, middle_name, last_name, is_faculty)
 VALUES
- ('student1.user@lib.com', 'password123', 'Jamie', NULL, 'Carter', 0),
- ('student2.user@lib.com', 'password123', 'Priya', NULL, 'Nair', 0),
- ('faculty.user@lib.com', 'password123', 'Michael', NULL, 'Chen', 1);
+ ('student1.user@lib.com', {{SEED_USER_PASSWORD}}, 'Jamie', NULL, 'Carter', 0),
+ ('student2.user@lib.com', {{SEED_USER_PASSWORD}}, 'Priya', NULL, 'Nair', 0),
+ ('faculty.user@lib.com', {{SEED_USER_PASSWORD}}, 'Michael', NULL, 'Chen', 1);

c82ad27219c03a85c1df7207288163e361adbfac
Sahan Wickramaratne <git@sahanwick.anonaddy.com>
2026-04-18 03:37:20 -0500

    update dates

--- database/data/transactions.sql
+++ database/data/transactions.sql
@@ -1,4 +1,4 @@
--- sample-base-date: 2026-04-14
+-- sample-base-date: 2026-04-18
 -- sample-shift-days: 0
 -- Borrow transactions
 
@@ -26,14 +26,14 @@ SET @faculty_id = (
 -- Student user has borrowed some items (not overdue)
 -- sample-shift-days-next: 0
 INSERT INTO borrow (item_id, user_id, checkout_date, due_date, return_date)
-SELECT item_id, @student1_id, '2026-04-11 10:00:00', '2026-04-18 10:00:00', NULL
+SELECT item_id, @student1_id, '2026-04-15 10:00:00', '2026-04-22 10:00:00', NULL
 FROM item
 WHERE title = 'To Kill a Mockingbird';
 
 -- Student user has a book due tomorrow
 -- sample-shift-days-next: 0
 INSERT INTO borrow (item_id, user_id, checkout_date, due_date, return_date)
-SELECT item_id, @student1_id, '2026-04-07 12:00:00', '2026-04-15 12:00:00', NULL
+SELECT item_id, @student1_id, '2026-04-11 12:00:00', '2026-04-19 12:00:00', NULL
 FROM item
 WHERE title = 'Pride and Prejudice';
 
@@ -41,13 +41,13 @@ WHERE title = 'Pride and Prejudice';
 
 -- sample-shift-days-next: -7
 INSERT INTO borrow (item_id, user_id, checkout_date, due_date, return_date)
-SELECT item_id, @student2_id, '2026-03-09 09:15:00', '2026-03-16 09:15:00', NULL
+SELECT item_id, @student2_id, '2026-03-06 09:15:00', '2026-03-13 09:15:00', NULL
 FROM item
 WHERE title = 'The Great Gatsby';
 
 -- sample-shift-days-next: -7
 INSERT INTO borrow (item_id, user_id, checkout_date, due_date, return_date)
-SELECT item_id, @student2_id, '2026-03-11 11:00:00', '2026-03-18 11:00:00', NULL
+SELECT item_id, @student2_id, '2026-03-08 11:00:00', '2026-03-15 11:00:00', NULL
 FROM item
 WHERE title = 'Pride and Prejudice';
 
@@ -55,12 +55,12 @@ WHERE title = 'Pride and Prejudice';
 
 -- sample-shift-days-next: -14
 INSERT INTO borrow (item_id, user_id, checkout_date, due_date, return_date)
-SELECT item_id, @faculty_id, '2026-02-08 13:20:00', '2026-02-22 13:20:00', NULL
+SELECT item_id, @faculty_id, '2026-01-29 13:20:00', '2026-02-12 13:20:00', NULL
 FROM item
 WHERE title = 'The Hobbit';
 
 -- sample-shift-days-next: -14
 INSERT INTO borrow (item_id, user_id, checkout_date, due_date, return_date)
-SELECT item_id, @faculty_id, '2026-02-11 16:45:00', '2026-02-25 16:45:00', NULL
+SELECT item_id, @faculty_id, '2026-02-01 16:45:00', '2026-02-15 16:45:00', NULL
 FROM item
 WHERE title = 'Dell Latitude 5440 Laptop';

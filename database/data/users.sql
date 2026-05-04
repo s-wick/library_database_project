48596d09c819ea15cb3ed4377adc92e83f96f868
Sahan Wickramaratne <git@sahanwick.anonaddy.com>
2026-05-04 15:46:53 -0500

    Renamed jamie to jeffrrey

--- database/data/users.sql
+++ database/data/users.sql
@@ -7,6 +7,6 @@ VALUES
 
 INSERT INTO user_account (email, password, first_name, middle_name, last_name, is_faculty)
 VALUES
- ('student1.user@lib.com', {{SEED_USER_PASSWORD}}, 'Jamie', NULL, 'Carter', 0),
+ ('student1.user@lib.com', {{SEED_USER_PASSWORD}}, 'Jeffrey', NULL, 'Carter', 0),
  ('student2.user@lib.com', {{SEED_USER_PASSWORD}}, 'Priya', NULL, 'Nair', 0),
  ('faculty.user@lib.com', {{SEED_USER_PASSWORD}}, 'Michael', NULL, 'Chen', 1);
